414a045f60c0528eeb529f122f080bef0103b1db
Alex Rothenberg <alex@alexrothenberg.com>
2020-07-10 11:39:52 -0400

    added some extensions for b2 allergy and med stuff

--- lib/ccd/templates/allergy_intolerance_observation_template.rb
+++ lib/ccd/templates/allergy_intolerance_observation_template.rb
@@ -10,10 +10,9 @@ module Ccd::AllergyIntoleranceObservationTemplate
       constraint 'mood_code', {:cardinality=>"1..1", :value=>"EVN"}
       
       # SHALL contain exactly one [1..1] templateId (CONF:7381) such that it
-      constraint 'template_id', {:cardinality=>"1..1"}
-      
       # SHALL contain exactly one [1..1] @root="2.16.840.1.113883.10.20.22.4.7" (CONF:10488).
-      constraint 'template_id.root', {:cardinality=>"1..1", :value=>"2.16.840.1.113883.10.20.22.4.7"}
+      constraint 'template_id', {:cardinality=>"1..1", :value=>{:root=>"2.16.840.1.113883.10.20.22.4.7"}}
+      constraint 'template_id', {:cardinality=>"1..1", :value=>{:root=>"2.16.840.1.113883.10.20.22.4.7", :extension=>"2014-06-09"}}
       
       # SHALL contain at least one [1..*] id (CONF:7382).
       constraint 'id', {:cardinality=>"1..*"}
